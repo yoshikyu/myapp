a0f41ff163d112e7cfa0f4f5a9e20c026b06db40
yoshikyu <yoshikiki1109@yahoo.co.jp>
2019-10-22 17:24:52 +0900

    環境周り修正

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/v1/', indexRouter);
+app.use('/', indexRouter);
 app.use('/v1/users', usersRouter);
 
 // catch 404 and forward to error handler
